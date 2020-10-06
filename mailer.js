4b92865c4859c42fa212ca622e2bdd51297d74c0
iiyass <proilyass@outlook.fr>
2020-10-06 21:44:58 +0100

    First commit

---
+++ mailer.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const router = new express.Router();
+const nodemailer = require("nodemailer");
+const contactAddress = process.env.CONTACT_ADDRESS || "ilyass1.ib@gmail.com";
+console.log(process.env.GMAIL_ADDRESS, process.env.GMAIL_PASSWORD);
+const mailer = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: process.env.GMAIL_ADDRESS,
+    pass: process.env.GMAIL_PASSWORD,
+  },
+});
+
+router.post("/contact", async (req, res) => {
+  await mailer
+    .sendMail({
+      from: req.body.email,
+      to: [contactAddress],
+      name: req.body.fullname,
+      subject: req.body.object || "[No subject]",
+      html:
+        `ClientName:${req.body.fullname}  ClientEmail:${req.body.email} Offer_Subject:${req.body.object}` ||
+        "[No subject]",
+    })
+    .then((res) => res.json({ success: true }))
+    .catch((e) => res.json({ error: e }));
+});
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const MailerRoute = require("./mailer");
+app.use(bodyParser.json());
+app.use(cors());
+app.use(MailerRoute);
+app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
