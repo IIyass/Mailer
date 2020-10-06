7c893728e85cef136b3aacc0b6cc8adfe58806fa
iiyass <proilyass@outlook.fr>
2020-10-06 23:46:45 +0100

    Adding Netlify Sett

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const serverless = require("serverless-http");
 const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,4 +8,10 @@ const MailerRoute = require("./mailer");
 app.use(bodyParser.json());
 app.use(cors());
 app.use(MailerRoute);
-app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+app.use("/.netlify/functions/server", MailerRoute);
+app.use("/", (req, res) =>
+  res.sendFile(path.join(__dirname, "../dist/index.html"))
+);
+
+module.exports = app;
+module.exports.handler = serverless(app);
