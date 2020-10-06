bffd01124ea1a550ea2283d96fefcb65be5db7d2
iiyass <proilyass@outlook.fr>
2020-10-07 00:23:26 +0100

    Configure Netlify

--- src/server.js
+++ src/server.js
@@ -3,15 +3,11 @@ const bodyParser = require("body-parser");
 const serverless = require("serverless-http");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || 3000;
 const MailerRoute = require("./mailer");
 app.use(bodyParser.json());
 app.use(cors());
-app.use(MailerRoute);
+
 app.use("/.netlify/functions/server", MailerRoute);
-app.use("/", (req, res) =>
-  res.sendFile(path.join(__dirname, "../dist/index.html"))
-);
 
 module.exports = app;
 module.exports.handler = serverless(app);
